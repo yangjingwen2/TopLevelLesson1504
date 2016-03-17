56780e351ac1225e4074ff9ff225a54b5c4df056
yangjw <yangwen8701@126.com>
2016-03-17 11:57:40 +0800

    添加HeaderView

---
+++ recyclerviewdemo/src/main/java/com/example/yangjw/recyclerviewdemo/HeaderRecyclerViewActivity.java
@@ -0,0 +1,98 @@
+package com.example.yangjw.recyclerviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HeaderRecyclerViewActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    //数据源
+    private List<Integer> bitmapList = new ArrayList<>();
+    private MyAdatper myAdatper;
+    View headerView;
+
+    public static final int POSITION_ITEM = 0;
+    public static final int HEADER_VIEW_TYPE = 1;
+    public static final int OTHER_VIEW_TYPE = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_header_recycler_view);
+        mRecyclerView = (RecyclerView) findViewById(R.id.header_recycler);
+        setupData();
+        //设置布局管理器
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //适配器
+        myAdatper = new MyAdatper();
+        mRecyclerView.setAdapter(myAdatper);
+
+        headerView = LayoutInflater.from(HeaderRecyclerViewActivity.this).inflate(R.layout.header_view, null);
+    }
+
+    //设置数据源信息
+    private void setupData() {
+        bitmapList.add(R.drawable.a1);
+        bitmapList.add(R.drawable.a2);
+        bitmapList.add(R.drawable.a3);
+        bitmapList.add(R.drawable.a4);
+    }
+
+    class MyAdatper extends RecyclerView.Adapter<MyAdatper.ViewHolder> {
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            Log.d("demo","--" + viewType);
+            if (viewType == HEADER_VIEW_TYPE) {
+                return new ViewHolder(headerView);
+            }
+            View view = LayoutInflater.from(HeaderRecyclerViewActivity.this).inflate(R.layout.recycler_item, null);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            if (position == POSITION_ITEM) {
+                return;
+            }
+            holder.imageView.setImageResource(bitmapList.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return bitmapList.size() + 1;
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            //下标为0的Item的ViewType为1，其他item的都为ViewType0
+            if (position == POSITION_ITEM) {
+                return HEADER_VIEW_TYPE;
+            }
+            return OTHER_VIEW_TYPE;
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+
+            public ImageView imageView;
+            public ViewHolder(View itemView) {
+                super(itemView);
+                if (headerView == itemView) {
+                    return;
+                }
+                imageView = (ImageView) itemView.findViewById(R.id.item_imageview);
+            }
+        }
+    }
+}
--- recyclerviewdemo/src/main/java/com/example/yangjw/recyclerviewdemo/MainActivity.java
+++ recyclerviewdemo/src/main/java/com/example/yangjw/recyclerviewdemo/MainActivity.java
@@ -10,12 +10,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jp.wasabeef.recyclerview.adapters.SlideInLeftAnimationAdapter;
+import jp.wasabeef.recyclerview.animators.OvershootInRightAnimator;
+import jp.wasabeef.recyclerview.animators.SlideInDownAnimator;
+import jp.wasabeef.recyclerview.animators.SlideInLeftAnimator;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private MyRecyclerAdapter myRecyclerAdapter;
+
+    private List<Integer> dataList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,17 +35,77 @@ public class MainActivity extends AppCompatActivity {
         //初始化
         mRecyclerView = (RecyclerView) findViewById(R.id.main_recycler_view);
         //指定一个布局管理器
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        mRecyclerView.setItemAnimator(new SlideInDownAnimator());
+        mRecyclerView.getItemAnimator().setAddDuration(1000);
+        mRecyclerView.getItemAnimator().setRemoveDuration(1000);
         mRecyclerView.addItemDecoration(new HorizontalDividerItemDecoration.Builder(this).color(Color.RED).build());
         //关联适配器
         myRecyclerAdapter = new MyRecyclerAdapter();
-        mRecyclerView.setAdapter(myRecyclerAdapter);
+        SlideInLeftAnimationAdapter slideInLeftAnimationAdapter = new SlideInLeftAnimationAdapter(myRecyclerAdapter);
+        mRecyclerView.setAdapter(slideInLeftAnimationAdapter);
 
+        //动画
+        //1、当删除和增加Item时的动画
+        //2、滚动的动画
+
+
+        dataList.add(R.drawable.a1);
+        dataList.add(R.drawable.a2);
+        dataList.add(R.drawable.a3);
+        dataList.add(R.drawable.a4);
+
+
+    }
+
+    public void click(View view) {
+        switch (view.getId()) {
+            case R.id.add:
+                dataList.add(1,R.drawable.a2);
+                myRecyclerAdapter.notifyItemInserted(1);
+                break;
+            case R.id.delete:
+                dataList.remove(1);
+                myRecyclerAdapter.notifyItemRemoved(1);
+                break;
+        }
     }
 
 
     //Recycler的适配器
     class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.ViewHolder> {
+
+
+        class MyListener implements View.OnClickListener {
+
+//            private int position;
+//
+//            public void setPosition(int position) {
+//                this.position = position;
+//            }
+
+            @Override
+            public void onClick(View v) {
+                Object tag = v.getTag();
+                Toast.makeText(MainActivity.this,"---" + tag,Toast.LENGTH_LONG).show();
+            }
+        }
+
+        MyListener myListener = new MyListener();
+
+//        View.OnClickListener onClickListener = new View.OnClickListener() {
+//
+//            private int position;
+//
+//            public void setPosition(int position) {
+//                this.position = position;
+//            }
+//
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        };
         /**
          * 创建ViewHolder对象,并且对Item的布局进行初始化
          * @param parent
@@ -56,8 +127,14 @@ public class MainActivity extends AppCompatActivity {
          */
         @Override
         public void onBindViewHolder(ViewHolder holder, int position) {
-            holder.imageView.setImageResource(R.drawable.ic_launcher);
+            holder.imageView.setImageResource(dataList.get(position));
+//            myListener.setPosition(position);
+            holder.imageView.setTag(position);
+            holder.imageView.setOnClickListener(myListener);
         }
+
+
+
 
         /**
          * 返回Item的个数---也数据数据源的大小
@@ -65,7 +142,7 @@ public class MainActivity extends AppCompatActivity {
          */
         @Override
         public int getItemCount() {
-            return 10;
+            return dataList.size();
         }
 
         /**
@@ -77,6 +154,12 @@ public class MainActivity extends AppCompatActivity {
             public ViewHolder(View itemView) {
                 super(itemView);
                 imageView = (ImageView) itemView.findViewById(R.id.item_imageview);
+//                imageView.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        Toast.makeText(MainActivity.this,"ddddd",Toast.LENGTH_LONG).show();
+//                    }
+//                });
             }
         }
     }
